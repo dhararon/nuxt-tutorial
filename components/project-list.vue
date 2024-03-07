07cf7799caa0682f9c8374c4666f3bec62b8243f
Dharwin Perez <dharwin@getfairplay.com>
2024-03-07 00:46:16 -0600

    feat: Add fetch support

--- components/project-list.vue
+++ components/project-list.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        Projects
-    </div>
+    <p class="mb-10">Take a  look at pokemon list</p>
+    <section v-if="pending">Loading ....</section>
+    <section v-else-if="error">Something wnet wrong ... Try again.</section>
+    <section v-else>
+        <ul class="grid grid-cols-1 gap-4">
+            <li v-for="poke in data.results" :key="poke.id" 
+            class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono">
+            
+            <a :href="poke.url">
+                <div class="font-semiblod"> {{ poke.name }}</div>
+            </a>
+            </li>
+        </ul>
+    </section>
 </template>
+
+<script setup>
+const { error, pending, data } = await useFetch('https://pokeapi.co/api/v2/pokemon/')
+console.log(data)
+</script>
